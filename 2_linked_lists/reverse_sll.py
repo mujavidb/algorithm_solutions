94f822cf1324c6e17e21a7d5c46d40ea3c657747
Mujavid Bukhari <sirmuj@gmail.com>
2016-03-30 14:13:57 -0700

    Add linear reverse linked-list solution

--- 2_linked_lists/reverse_sll.py
+++ 2_linked_lists/reverse_sll.py
@@ -14,7 +14,7 @@ def addNode(root, value):
 
 def printList(root):
 	while root:
-		print " " + str(root.value),
+		print str(root.value) + " ",
 		root = root.next
 	print
 
@@ -28,6 +28,21 @@ def reverseLL(root):
 		addNode(new, array.pop())
 	return new
 
+#O(n) recursive possible
+#O(n) with two pointers possible
+
+def linearReverse(root):
+	if root.next:
+		head = root
+		current = root.next
+		while current:
+			temp = current
+			root.next = current.next
+			current = root.next
+			temp.next = head
+			head = temp
+		return head
+
 root = Node(5)
 addNode(root, 6)
 addNode(root, 7)
@@ -35,5 +50,5 @@ addNode(root, 8)
 addNode(root, 9)
 addNode(root, 10)
 printList(root)
-root = reverseLL(root)
-printList(root)
+fun = linearReverse(root)
+printList(fun)
